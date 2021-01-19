dfdcea463189f790b351b1564a81b18e98fe3f01
Leandro <leandro.satorii@gmail.com>
2021-01-19 16:48:27 -0300

    update src/e_commerce/urls.py inserting settings,static imports and put if settings.DEBUG

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.contrib import admin
 from django.urls import path
 
@@ -24,7 +27,10 @@ urlpatterns = [
 	path('', home_page),
 	path('about/', about_page),
 	path('contact/', contact_page),
-    path('login/', login_page),
-    path('register/', register_page),
+        path('login/', login_page),
+        path('register/', register_page),
 	path('admin/', admin.site.urls),
 ]
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
