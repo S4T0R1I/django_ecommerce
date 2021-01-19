1af96345ec7c613fb47cf465c636b0f36133d98e
Leandro <leandro.satorii@gmail.com>
2021-01-19 10:01:37 -0300

    put imports and insert in urlpatterns the about_page and contact_page in src/urls.py, and update .gitignore

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -13,11 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
-from .views import home_page
+from .views import home_page, about_page, contact_page
 
 urlpatterns = [
-    path('', home_page),
-    path('admin/', admin.site.urls),
+        path('', home_page),
+        path('about/', about_page),
+	    path('contact/', contact_page),
+        path('admin/', admin.site.urls),
 ]
